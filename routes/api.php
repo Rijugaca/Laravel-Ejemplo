f96e0f1d08041cca72a9033381cbd14c90912bce
Rijugaca <97418748+Rijugaca@users.noreply.github.com>
2022-04-04 19:14:20 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,8 @@ use Illuminate\Support\Facades\Route;
 | API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
+| Herebbbb i
+s where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+/* Git hub*/
